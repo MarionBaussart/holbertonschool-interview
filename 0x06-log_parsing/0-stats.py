e16f5524660fe19be99a66be584c500e1bc94b91
MarionBaussart <marion.baussart@gmail.com>
2023-01-25 13:19:35 +0000

    sorted dict, get arg

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,8 +6,9 @@ import sys
 
 nb_line_read = 0
 total_size = 0
-status_code_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
+status_code_line = 0
+status_code_dict = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
 try:
     for line in sys.stdin:
@@ -17,16 +18,24 @@ try:
         size_line = int(splited_line[-1])
         total_size += size_line
 
-        # nb of line by status code
-        status_code_line = splited_line[-2]
-        for status_code in sorted(status_code_dict):
-            if status_code_line == status_code:
-                status_code_dict[status_code_line] += 1
-                status_code_dict
-
         nb_line_read += 1
 
+        # nb of line by status code
+        try:
+            status_code_line = int(splited_line[-2])
+
+            if status_code_line not in status_code_dict.keys():
+                pass
+            for status_code in sorted(status_code_dict):
+                if status_code_line == status_code:
+                    status_code_dict[status_code_line] += 1
+
+        except ValueError:
+            pass
+
         if nb_line_read % 10 == 0:
+
+
             print("File size: {}".format(total_size))
             for status_code in sorted(status_code_dict):
                 if status_code_dict[status_code] != 0:
